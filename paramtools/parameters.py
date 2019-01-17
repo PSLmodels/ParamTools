720921414a1bf41c23d7c9284788951403c483cd
hdoupe <henrymdoupe@gmail.com>
2019-01-17 13:45:51 -0500

    Add comments to Parameters class

--- paramtools/parameters.py
+++ paramtools/parameters.py
@@ -29,11 +29,13 @@ class Parameters:
         """
         Method to deserialize and validate parameter adjustments.
         `params_or_path` can be a file path or a `dict` that has not been
-        fully deserialized.
+        fully deserialized. The adjusted values replace the current values
+        stored in the correspondig parameter attributes.
 
-        Returns: serialized data.
-
-        Throws: `marshmallow.exceptions.ValidationError` if data is not valid.
+        Raises:
+            marshmallow.exceptions.ValidationError if data is not valid.
+            ParameterUpdateException if dimension values do not match at
+                least one existing value item's corresponding dimension values.
         """
         if isinstance(params_or_path, str) and os.path.exists(params_or_path):
             params = utils.read_json(params_or_path)
@@ -67,10 +69,24 @@ class Parameters:
     def get(self, param, **kwargs):
         """
         Query a parameter's values along dimensions specified in `kwargs`.
+
+        Returns: [{"value": val, "dim0": ..., }]
+
+        Raises:
+            KeyError if queried dimension is not used by this parameter.
+            AttributeError if parameter does not exist.
         """
         return self._get(param, True, **kwargs)
 
     def specification(self, meta_data=False, **kwargs):
+        """
+        Query value(s) of all parameters along dimensions specified in
+        `kwargs`. If `meta_data` is `True`, then parameter attributes
+        are included, too.
+
+        Returns: serialized data of shape
+            {"param_name": [{"value": val, "dim0": ..., }], ...}
+        """
         all_params = OrderedDict()
         for param in self._validator_schema.fields:
             result = self._get(param, False, **kwargs)
@@ -82,6 +98,11 @@ class Parameters:
         return all_params
 
     def format_errors(self, validation_error, compress_errors=True):
+        """
+        Format error messages from ValidationError instance. If
+        `compress_errors` is `True`, all messages are collected and
+        stored in a list.
+        """
         if compress_errors:
             for param, messages in validation_error.messages.items():
                 if param in self.errors:
@@ -97,6 +118,8 @@ class Parameters:
         Private method for querying a parameter along some dimensions. If
         exact_match is True, all values in `kwargs` must be equal to the
         corresponding dimension in the parameter's "value" dictionary.
+
+        Returns: [{"value": val, "dim0": ..., }]
         """
         value = getattr(self, param)["value"]
         ret = []
@@ -108,6 +131,16 @@ class Parameters:
         return ret
 
     def _update_param(self, param, new_values):
+        """
+        Private method for updating the current parameter values with those
+        specified by the adjustment. The values that need to be updated are
+        chosen by finding all value items with dimension values matching
+        the dimension values specified in the adjustment.
+
+        Raises:
+            ParameterUpdateException if dimension values do not match at
+                least one existing value item's corresponding dimension values.
+        """
         curr_vals = getattr(self, param)["value"]
         for i in range(len(new_values)):
             matched_at_least_once = False
