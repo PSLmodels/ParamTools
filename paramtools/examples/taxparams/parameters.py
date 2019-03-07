556d8a88b170d6dec9bafc430f6a4a3b451f3a2e
hdoupe <henrymdoupe@gmail.com>
2019-03-07 16:22:31 -0500

    Write a chunk of the doc

--- paramtools/examples/taxparams/parameters.py
+++ paramtools/examples/taxparams/parameters.py
@@ -19,12 +19,11 @@ class IndexedParameters(paramtools.Parameters):
         min_allowed_year = min(self._stateless_dim_mesh["year"])
         max_allowed_year = max(self._stateless_dim_mesh["year"])
         adjustment = defaultdict(list)
+        spec = self.specification(use_state=False, meta_data=True)
         if params_to_extend is None:
-            param_data = self._data
+            param_data = spec
         else:
-            param_data = {
-                param: self._data[param] for param in params_to_extend
-            }
+            param_data = {param: spec[param] for param in params_to_extend}
 
         def get_vo_lookup(vos, dims):
             qh = {}
@@ -33,7 +32,7 @@ class IndexedParameters(paramtools.Parameters):
             return qh
 
         for param, data in param_data.items():
-            max_year = data["value"][-1]["year"]
+            max_year = max(map(lambda x: x["year"], data["value"]))
             if max_year == max_allowed_year:
                 continue
             max_year = data["value"][-1]["year"]
@@ -55,17 +54,17 @@ class IndexedParameters(paramtools.Parameters):
                     range(max_year + 1, max_allowed_year + 1)
                 ):
                     for vo in value_objects:
-                        hash_key = tuple(
+                        dim_values = tuple(
                             vo[dim_name] for dim_name in dims_to_match
                         )
-                        v = vo_lookup[hash_key] * (
+                        v = vo_lookup[dim_values] * (
                             1 + rates[max_year - min_allowed_year + ix]
                         )
                         v = np.round(v, 2) if v < 9e99 else 9e99
                         adjustment[param].append(
                             dict(vo, **{"year": year, "value": v})
                         )
-                        vo_lookup[hash_key] = v
+                        vo_lookup[dim_values] = v
             else:
                 for year in range(max_year, max_allowed_year + 1):
                     for vo in value_objects:
@@ -84,9 +83,6 @@ class IndexedParameters(paramtools.Parameters):
         self.adjust(params)
         self.extend(params_to_extend=list(params.keys()))
         self.array_first = True
-
-    def indexing_rates(self, param=None):
-        pass
 
 
 class CompatibleDataSchema(Schema):
