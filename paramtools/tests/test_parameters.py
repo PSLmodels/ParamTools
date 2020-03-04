ec89173b3b2e042d22860b5b610edebfcf79adea
hdoupe <henrymdoupe@gmail.com>
2020-03-04 18:25:06 -0500

    Add select less than function

--- paramtools/tests/test_parameters.py
+++ paramtools/tests/test_parameters.py
@@ -1905,3 +1905,39 @@ class TestIndex:
                     "indexed_param": [{"d0": 3, "value": 8}],
                 }
             )
+
+
+class TestSelect:
+    def test_select_lt(self):
+        class Params(Parameters):
+            defaults = {
+                "schema": {
+                    "labels": {
+                        "d0": {"type": "int"},
+                        "d1": {
+                            "type": "str",
+                            "validators": {
+                                "choice": {"choices": ["hello", "world"]}
+                            },
+                        },
+                    }
+                },
+                "param": {
+                    "title": "",
+                    "description": "",
+                    "type": "int",
+                    "value": [
+                        {"d0": 1, "d1": "hello", "value": 1},
+                        {"d0": 1, "d1": "world", "value": 1},
+                        {"d0": 2, "d1": "hello", "value": 1},
+                        {"d0": 3, "d1": "world", "value": 1},
+                    ],
+                },
+            }
+
+        params = Params()
+        assert list(params.select_lt("param", False, d0=3)) == [
+            {"d0": 1, "d1": "hello", "value": 1},
+            {"d0": 1, "d1": "world", "value": 1},
+            {"d0": 2, "d1": "hello", "value": 1},
+        ]
